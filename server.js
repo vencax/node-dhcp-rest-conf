bef648e6c3682023453c49125703d2890f896ca9
Vaclav Klecanda <vencax77@gmail.com>
2014-11-30 21:31:11 +0100

    cors

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+
 require('coffee-script/register');
 var express = require('express');
+var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
+var cors = require('cors');
 
 var port = process.env.PORT || 8080;
 
+var corsOptions = {
+  origin: process.env.CORSORIGIN
+};
+
 // create API
 var api = express();
+// enable CORS
+api.options('*', cors(corsOptions));
 api.use(bodyParser.json())
 // secure it with JWT
 api.use(expressJwt({secret: process.env.SERVER_SECRET}));
 // create the routes
 api.use(require('./lib/app'));
 
-app.listen(port, function() {
+api.listen(port, function() {
   console.log('gandalf do magic on ' + port);
 });
